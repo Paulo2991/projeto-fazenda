f137dfeccdf16fe50247971646d57c2f1d75da41
Paulo2991 <paulovaz38034@gmail.com>
2024-05-18 14:16:06 -0300

    Ajustando projeto

---
+++ js/script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const sobre = document.getElementById('sobre');
+	const descriptionModal = document.querySelectorAll('.descriptionModal');
+	const myModal = document.getElementById('myModal');
+	const closeButtons = document.querySelectorAll('.close');
+
+	function eventosButton(button,type){
+		if(type === true){
+			button.addEventListener('click',function(){
+				cardDescription();
+				myModal.style.display = 'block';
+			});
+		}else{
+			button.addEventListener("click", function () {
+				myModal.style.display = "none";
+			});
+		}
+	}
+
+	function mostrarEventos(button,type){
+		button.forEach(function(){
+			if(type === true){
+
+			}
+		});
+	}
+
+	window.addEventListener('click', function(event){
+		if(event.target === myModal){
+			eventosButton(myModal,false)
+		}
+	});
+
+	closeButtons.forEach(function () {
+		eventosButton(myModal,false)
+	});
+
+	function cardDescription(){
+		let modalDescription = `
+		<span class="close">&times;</span>
+		<p>
+		O seu terreno dos sonhos está te esperando. Vai deixar passar essa oportunidade? Tudo começa com um terreno. As grandes oportunidades surgem a partir dele. Lugar tranquilo que permite você caminhe todas as manhãs de forma agradável e uma belissima cachoeira. Próximo da sede e 1 ribeirão com águas límpidas e cristalinas rodeando parte da prorpriedade.
+		</p>`;
+
+		descriptionModal.forEach(function (modalContent) {
+			modalContent.innerHTML = modalDescription;
+		});
+	}
+
+	eventosButton(sobre,true);
+});
+
